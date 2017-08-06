6de2398e7358e59a774ff5ff0e75d2f0c39b0991
Nhím Còi <thuyntt32@wru.vn>
2017-08-07 01:53:10 +0700

    add

--- app/src/main/java/com/example/nhimcoi/clothesnails/View/HomePage/HomePageActivity.java
+++ app/src/main/java/com/example/nhimcoi/clothesnails/View/HomePage/HomePageActivity.java
@@ -1,15 +1,56 @@
 package com.example.nhimcoi.clothesnails.View.HomePage;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ExpandableListView;
 
 import com.example.nhimcoi.clothesnails.R;
 
 public class HomePageActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    ExpandableListView expandableListView;
+    Menu menu;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    AppBarLayout appBarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        tabLayout=(TabLayout)findViewById(R.id.tabs);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawlayout);
+        expandableListView = (ExpandableListView)findViewById(R.id.epMenu);
+        appBarLayout=(AppBarLayout)findViewById(R.id.appBar);
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+        setTitle("");
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //đồng bộ dữ liệu
+        actionBarDrawerToggle.syncState();
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        viewPager.setAdapter(adapter);
+//        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 }
